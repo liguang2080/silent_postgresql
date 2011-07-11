6b04b878f0a8a4f0f1f7f960d0e3cb910fe6e7d0
lg2046 <lg2046@gmail.com>
2011-07-11 15:56:23 +0800

    railtie

--- lib/silent_postgresql.rb
+++ lib/silent_postgresql.rb
@@ -1,3 +1,25 @@
-module SilentPostgresql
-  # Your code goes here...
+if Rails.env.development? || Rails.env.test?
+
+  module SilentPostgresql
+    SILENT_METHODS = %w(table table_exists? indexs column_definitions pk_and_sequence_for last_insert_id)
+
+    def self.included(base)
+      SILENT_METHODS.each do |m|
+        base.send :alias_method_chain, m, :silencer
+      end
+    end
+
+    SILENT_METHODS.each do |m|
+      aliased_method, punctuation = m.gsub(/([?!=])$/, ''), $1
+      class_eval <<-METHOD
+      def #{aliased_method}_with_silencer#{punctuation}(*args)
+        @logger.silence do
+          #{aliased_method}_without_silencer#{punctuation}(*args)
+        end
+      end
+      METHOD
+    end
+  end
+
+  require "silent_postgresql/railtie"
 end
---
+++ lib/silent_postgresql/railtie.rb
@@ -0,0 +1,14 @@
+module SilentPostgres
+  
+  class Railtie < Rails::Railtie
+    initializer "silent_postgres.insert_into_active_record" do
+      ActiveSupport.on_load :active_record do
+        begin
+          ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, SilentPostgres)
+        rescue NameError
+        end
+      end
+    end
+  end
+
+end
