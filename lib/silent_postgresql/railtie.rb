65fbd4ed84db9684ead765035f901accf569d2ca
lg2046 <lg2046@gmail.com>
2011-07-11 16:52:55 +0800

    wo yun

--- lib/silent_postgresql/railtie.rb
+++ lib/silent_postgresql/railtie.rb
@@ -4,6 +4,7 @@ module SilentPostgres
     initializer "silent_postgres.insert_into_active_record" do
       ActiveSupport.on_load :active_record do
         begin
+          puts "google"
           ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, SilentPostgres)
         rescue NameError
         end
